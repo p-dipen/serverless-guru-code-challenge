ce84056f6148e68601c06e8c636d455db8d59ac5
Dipen Parihar <parihar.dipen@gmail.com>
2023-09-11 22:33:55 -0400

    Changes for stages

--- serverless.ts
+++ serverless.ts
@@ -13,6 +13,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    stage: "${opt:stage, 'dev'}",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
